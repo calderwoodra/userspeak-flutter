f4ef1cfb379ac1b91501c545d07c79bf597b1b56
Allen Calderwood <calderwoodra1113@gmail.com>
2021-03-22 13:50:41 -0700

    messing around.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:userspeak/main.dart';
+import 'package:userspeak/demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
